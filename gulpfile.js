eec34e0bf3b6e6b82dfab35b60332cdec9f8a561
Barnz <hi@barnz.dev>
2021-10-12 22:43:44 +0100

    add cleanup task

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,21 @@
 require('dotenv').config();
 const _PROD = process.env.MODE === 'production' ? true : false;
 
-const { src, dest, watch, series } = require('gulp');
+const { src, dest, watch, series, parallel } = require('gulp');
 const gulpif = require('gulp-if');
 const sourcemaps = require('gulp-sourcemaps');
 const sass = require('gulp-sass')(require('sass'));
 const autoprefixer = require('gulp-autoprefixer');
 const cleanCSS = require('gulp-clean-css');
+const del = require('del');
 
 const outputLocation = './dist';
 const sassLocation = './src/css/main.scss';
 const htmlLocation = './src/html/index.html';
+
+function cleanup() {
+    return del([outputLocation + '/**/*']);
+}
 
 function compileSass() {
     return src(sassLocation)
@@ -35,5 +40,6 @@ function watchSource() {
     watch(htmlLocation, copyHtml);
 }
 
-exports.build = series(copyHtml, compileSass);
-exports.watch = series(copyHtml, compileSass, watchSource);
+exports.build = series(cleanup, parallel(copyHtml, compileSass));
+exports.watch = series(cleanup, copyHtml, compileSass, watchSource);
+exports.clean = cleanup;
