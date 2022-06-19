6db949117474145eb598c010a6600b40613e4310
barnz <hi@barnz.dev>
2022-06-19 21:45:22 +0100

    shouldHideEffect() tests

--- tests/projectItem.spec.ts
+++ tests/projectItem.spec.ts
@@ -2,15 +2,37 @@ import ResponsiveHelpers from '../themes/bdev/assets/scripts/helpers/responsive.
 import ProjectItemEffect from '../themes/bdev/assets/scripts/projectItem';
 
 describe('Project item', () => {
+  let rh: ResponsiveHelpers;
   let component: ProjectItemEffect;
 
   beforeEach(() => {
-    const helper = new ResponsiveHelpers();
+    rh = new ResponsiveHelpers();
     
-    component = new ProjectItemEffect(helper);
+    component = new ProjectItemEffect(rh);
   });
 
   test('should create', () => {
     expect(component).toBeDefined();
+  });
+
+  describe('shouldHideEffect()', () => {
+    it('should return true if detected as mobile', () => {
+      jest.spyOn(rh, 'isMobile').mockImplementation(() => true);
+
+      expect(component.shouldHideEffect()).toBe(true);
+    });  
+
+    it('should return true if detected as a tablet', () => {
+      jest.spyOn(rh, 'isTablet').mockImplementation(() => true);
+
+      expect(component.shouldHideEffect()).toBe(true);
+    });
+
+    it('should return false if not detected as a mobile or tablet', () => {
+      jest.spyOn(rh, 'isMobile').mockImplementation(() => false);
+      jest.spyOn(rh, 'isTablet').mockImplementation(() => false);
+
+      expect(component.shouldHideEffect()).toBe(false);
+    });
   });
 });
