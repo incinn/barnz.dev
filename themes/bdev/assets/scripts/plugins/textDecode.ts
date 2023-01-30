0bea38daf1b697db9d26d76f4915392213d7eb93
barnz <hi@barnz.dev>
2023-01-30 00:45:07 +0000

    cleanup

--- themes/bdev/assets/scripts/plugins/textDecode.ts
+++ themes/bdev/assets/scripts/plugins/textDecode.ts
@@ -1,7 +1,6 @@
 import Plugin from "../plugin";
 
 export default class TextDecode extends Plugin {
-  letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
   targets: NodeListOf<HTMLElement>;
 
@@ -10,12 +9,12 @@ export default class TextDecode extends Plugin {
     this.targets = document.querySelectorAll(".js-decode");
 
     if (!this.targets || this.targets.length < 1) {
-      this.init = () => console.info("No decode elements found");
+      this.init = () => console.info("No decode-effect elements found");
     }
   }
 
   init(): void {
-    this.targets.forEach((target) => {
+    this.targets.forEach((target: HTMLElement) => {
       this.handleEffect(target);
 
       target.addEventListener("mouseover", () =>
@@ -26,21 +25,20 @@ export default class TextDecode extends Plugin {
 
   reset(): void {}
 
-  handleEffect(el: HTMLElement, hover = false): void {
+  handleEffect(el: HTMLElement, infinite = false): void {
     if (el.classList.contains("animating")) return;
     el.classList.add("animating");
 
     const initialText = el.innerText;
     let iterations = 0;
 
-    if (hover) el.addEventListener("mouseout", () => (hover = false));
+    if (infinite) el.addEventListener("mouseout", () => (infinite = false));
 
     const interval = setInterval(() => {
       el.innerText = el.innerText
         .split("")
         .map((_letter, index) => {
-          if (index < iterations && !hover) return initialText[index];
-          return this.letters[Math.floor(Math.random() * 26)];
+          if (index < iterations && !infinite) return initialText[index];
           return this.letters[Math.floor(Math.random() * this.letters.length)];
         })
         .join("");
@@ -49,7 +47,7 @@ export default class TextDecode extends Plugin {
         el.classList.remove("animating");
         clearInterval(interval);
       }
-      if (!hover) iterations += initialText.length / 50;
+      if (!infinite) iterations += initialText.length / 50;
     }, 55);
   }
 }
