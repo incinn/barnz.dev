cb129037560c6a8ee1ef3509946a48b720c70e05
barnz <hi@barnz.dev>
2023-04-26 20:27:34 +0100

    fix title hierarchy on homepage

--- themes/bdev/assets/scripts/plugins/picker.ts
+++ themes/bdev/assets/scripts/plugins/picker.ts
@@ -1,5 +1,5 @@
-import feather = require("feather-icons");
-import Plugin from "../plugin";
+import feather = require('feather-icons');
+import Plugin from '../plugin';
 
 interface UserChangedResponse {
   text: string;
@@ -9,11 +9,11 @@ interface UserChangedResponse {
 const component = `
   <div class="picker__container">
     <div class="picker__inner">
-      <h1>
+      <h2>
         <span class="picker__titleText">Hate</span>
         <span class="picker__titleColor">#df1155</span>
         <span class="picker__titleTextPost">?</span>
-      </h1>
+      </h2>
       <p>
         Select an accent colour below, or choose your own with the picker on the right.
       </p>
@@ -24,7 +24,7 @@ const component = `
   </div>
 `;
 
-const presets = ["#26bf80", "#df1155", "#457dd3", "#c18f34", "#834ed3"];
+const presets = ['#26bf80', '#df1155', '#457dd3', '#c18f34', '#834ed3'];
 
 export default class Picker extends Plugin {
   container: HTMLElement;
@@ -33,49 +33,49 @@ export default class Picker extends Plugin {
 
   userChangedResponses: UserChangedResponse[] = [
     {
-      text: "Hmmm",
-      postText: "...",
+      text: 'Hmmm',
+      postText: '...',
     },
     {
-      text: "",
-      postText: "👀",
+      text: '',
+      postText: '👀',
     },
     {
-      text: "😍",
-      postText: "is much nicer!",
+      text: '😍',
+      postText: 'is much nicer!',
     },
     {
-      text: "🤢",
-      postText: "",
+      text: '🤢',
+      postText: '',
     },
     {
-      text: "",
-      postText: "if you insist...",
+      text: '',
+      postText: 'if you insist...',
     },
     {
-      text: "",
+      text: '',
       postText: "wouldn't be my first choice",
     },
     {
-      text: "",
-      postText: "❤️",
+      text: '',
+      postText: '❤️',
     },
     {
-      text: "",
-      postText: "reminds me of Comic Sans",
+      text: '',
+      postText: 'reminds me of Comic Sans',
     },
     {
-      text: "",
-      postText: "perfect!",
+      text: '',
+      postText: 'perfect!',
     },
   ];
 
   constructor() {
     super();
 
-    this.container = document.getElementById("picker");
+    this.container = document.getElementById('picker');
     if (!this.container) {
-      this.init = () => console.info("Picker did not start");
+      this.init = () => console.info('Picker did not start');
     }
   }
 
@@ -87,19 +87,19 @@ export default class Picker extends Plugin {
   }
 
   create(): void {
-    this.wrapperEl = document.createElement("aside");
-    this.wrapperEl.classList.add("picker");
+    this.wrapperEl = document.createElement('aside');
+    this.wrapperEl.classList.add('picker');
     this.wrapperEl.innerHTML = component;
 
-    const presetContainer = this.wrapperEl.querySelector(".picker__presets");
+    const presetContainer = this.wrapperEl.querySelector('.picker__presets');
     presets.forEach((val) => {
       const btn = this.createPresetButton(val);
-      btn.addEventListener("click", this.handlePresetClick.bind(this));
+      btn.addEventListener('click', this.handlePresetClick.bind(this));
       presetContainer.appendChild(btn);
     });
     presetContainer.appendChild(this.createPicker());
 
-    const pickerInner = this.wrapperEl.querySelector(".picker__inner");
+    const pickerInner = this.wrapperEl.querySelector('.picker__inner');
     const resetButton = this.createResetButton();
     pickerInner.appendChild(resetButton);
 
@@ -107,23 +107,23 @@ export default class Picker extends Plugin {
   }
 
   createPresetButton(colour: string): HTMLButtonElement {
-    const btn = document.createElement("button");
+    const btn = document.createElement('button');
     btn.dataset.colour = colour;
     btn.style.backgroundColor = colour;
-    btn.setAttribute("aria-label", `Set accent colour ${colour}`);
+    btn.setAttribute('aria-label', `Set accent colour ${colour}`);
 
     return btn;
   }
 
   createPicker(): HTMLElement {
-    const wrapper = document.createElement("div");
-    wrapper.classList.add("colorWrapper");
-    const picker = document.createElement("input");
-    picker.type = "color";
-    const icon = document.createElement("span");
+    const wrapper = document.createElement('div');
+    wrapper.classList.add('colorWrapper');
+    const picker = document.createElement('input');
+    picker.type = 'color';
+    const icon = document.createElement('span');
     icon.innerHTML = feather.icons.edit.toSvg();
 
-    picker.addEventListener("change", this.handleInputChange.bind(this));
+    picker.addEventListener('change', this.handleInputChange.bind(this));
 
     wrapper.appendChild(picker);
     wrapper.appendChild(icon);
@@ -133,28 +133,28 @@ export default class Picker extends Plugin {
 
   createResetButton(): HTMLButtonElement {
     const button = this.wrapperEl.querySelector(
-      ".picker__reset"
+      '.picker__reset'
     ) as HTMLButtonElement;
-    const icon = document.createElement("span");
-    icon.innerHTML = feather.icons["refresh-cw"].toSvg();
+    const icon = document.createElement('span');
+    icon.innerHTML = feather.icons['refresh-cw'].toSvg();
 
-    button.addEventListener("click", this.reset.bind(this));
+    button.addEventListener('click', this.reset.bind(this));
     button.appendChild(icon);
 
     return button;
   }
 
   loadValueFromStore(): string | null {
-    const savedValue = localStorage.getItem("accent");
+    const savedValue = localStorage.getItem('accent');
     return savedValue ? savedValue : null;
   }
 
   setValueInStore(): void {
-    localStorage.setItem("accent", this.color);
+    localStorage.setItem('accent', this.color);
   }
 
   reset(): void {
-    localStorage.removeItem("accent");
+    localStorage.removeItem('accent');
     window.location.reload();
   }
 
@@ -168,9 +168,9 @@ export default class Picker extends Plugin {
   }
 
   updateText(): void {
-    const colorText = this.wrapperEl.querySelector(".picker__titleColor");
-    const text = this.wrapperEl.querySelector(".picker__titleText");
-    const textPost = this.wrapperEl.querySelector(".picker__titleTextPost");
+    const colorText = this.wrapperEl.querySelector('.picker__titleColor');
+    const text = this.wrapperEl.querySelector('.picker__titleText');
+    const textPost = this.wrapperEl.querySelector('.picker__titleTextPost');
 
     colorText.innerHTML = this.color;
 
@@ -198,10 +198,10 @@ export default class Picker extends Plugin {
   }
 
   setColour(): void {
-    const root = document.querySelector(":root") as HTMLElement;
+    const root = document.querySelector(':root') as HTMLElement;
 
-    root.style.setProperty("--accent", this.color);
-    root.style.setProperty("--accent-alt", this.getContrastColour(this.color));
+    root.style.setProperty('--accent', this.color);
+    root.style.setProperty('--accent-alt', this.getContrastColour(this.color));
   }
 
   getContrastColour(color: string): string {
@@ -209,12 +209,12 @@ export default class Picker extends Plugin {
     const contrast =
       0.2126 * rgbColor[0] + 0.7152 * rgbColor[1] + 0.0722 * rgbColor[2];
 
-    return contrast >= 165 ? "#000000" : "#ffffff";
+    return contrast >= 165 ? '#000000' : '#ffffff';
   }
 
   hexToRgb(hex: string): number[] {
     // https://javascript.plainenglish.io/convert-hex-to-rgb-with-javascript-4984d16219c3
-    if (hex[0] === "#") hex = hex.slice(1);
+    if (hex[0] === '#') hex = hex.slice(1);
 
     const aRgbHex = hex.match(/.{1,2}/g);
     return [
