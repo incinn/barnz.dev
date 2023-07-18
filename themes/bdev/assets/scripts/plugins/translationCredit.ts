6626beba6fb47b236450ccfdd3afb01eaa8e3a49
barnz <hi@barnz.dev>
2023-07-19 00:47:08 +0100

    three hearts per click

--- themes/bdev/assets/scripts/plugins/translationCredit.ts
+++ themes/bdev/assets/scripts/plugins/translationCredit.ts
@@ -24,25 +24,36 @@ export default class TranslationCredit extends Plugin {
     if(button.classList.contains('voteNo')) {
       button.classList.add('voteNo--yes');
     }
+    
+    let i = 0;
+    this.createEffect(button);
+    const interval = setInterval(() => {
+      if(i === 1) clearInterval(interval);
 
-    const heart = this.createHeart();
-    heart.style.setProperty('--heart-random-1', this.randomNumber(-20, 10) + 'px');
-    heart.style.setProperty('--heart-random-2', this.randomNumber(-10, 20) + 'px');
-    heart.style.setProperty('--heart-random-3', this.randomNumber(-25, 25) + 'px');
-    heart.style.setProperty('--heart-random-4', this.randomNumber(-30, 30) + 'px');
-    button.appendChild(heart);
-
-    setTimeout(() => {
-      heart.remove();
-    }, 1000)
+      this.createEffect(button);
+      i++;
+    }, 200)
   }
 
-  createHeart(): HTMLElement {
+  createHeartElement(): HTMLElement {
     const heartEl = document.createElement('span');
     heartEl.innerHTML = '❤️';
     heartEl.classList.add('heart');
 
     return heartEl;
+  }
+
+  createEffect(button: HTMLButtonElement): void {
+    const heart = this.createHeartElement();
+    heart.style.setProperty('--heart-random-1', this.randomNumber(-25, 15) + 'px');
+    heart.style.setProperty('--heart-random-2', this.randomNumber(-15, 5) + 'px');
+    heart.style.setProperty('--heart-random-3', this.randomNumber(-25, 15) + 'px');
+    heart.style.setProperty('--heart-random-4', this.randomNumber(-15, 5) + 'px');
+    button.appendChild(heart);
+
+    setTimeout(() => {
+      heart.remove();
+    }, 1000)
   }
 
   randomNumber(min: number, max: number): number {
