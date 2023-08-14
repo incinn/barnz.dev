9b750849c4bcb725201883b2258b6934dac87e5c
barnz <hi@barnz.dev>
2023-08-14 20:36:13 +0100

    implement script to set env variables for latest git commit to be displayed in footer

---
+++ get-latest-commit.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+export HUGOxPARAMSxGITxLAST_COMMITxDATE=$(git log -1 --format=%cI)
+export HUGOxPARAMSxGITxLAST_COMMITxHASHLONG=$(git log -1 --format=%H)
+export HUGOxPARAMSxGITxLAST_COMMITxHASH=$(git log -1 --format=%h)
