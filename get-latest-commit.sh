e627de9fd786f60e90ec4a14f2afedc306d2f847
barnz <hi@barnz.dev>
2023-08-14 20:51:32 +0100

    echo out long hash to help with debugging/logs

--- get-latest-commit.sh
+++ get-latest-commit.sh
@@ -3,3 +3,4 @@
 export HUGOxPARAMSxGITxLAST_COMMITxDATE=$(git log -1 --format=%cI)
 export HUGOxPARAMSxGITxLAST_COMMITxHASHLONG=$(git log -1 --format=%H)
 export HUGOxPARAMSxGITxLAST_COMMITxHASH=$(git log -1 --format=%h)
+echo $HUGOxPARAMSxGITxLAST_COMMITxHASHLONG
